be68b90520fa1599b795323c34e9c073db37d14a
João Vicente Souto <joaovicentesouto@gmail.com>
2019-10-04 18:44:13 -0300

    R script to get apple reviews

--- scripts/apple_get_comments.r
+++ scripts/apple_get_comments.r
@@ -1,5 +1,3 @@
-devtools::install_github("amrrs/itunesr")
-
 library(itunesr)
 
 input   = read.csv("datasets/apple/AppleStore.csv")
@@ -10,17 +8,29 @@ dataset[["id"]]     <- 0
 dataset[["review"]] <- "Dummy"
 
 # Create dataset
+count <- 0
+total <- length(input$id)
 for (id in input$id)
 {
-    cat("Downloading ", id, " reviews")
+    count       <- (count + 1)
+    percentagem <- as.integer((count / total) * 100)
 
-    reviews <- getReviews(id, 'us', 1)
+    tryCatch({
+        reviews_list <- getReviews(id,'us',1)
 
-    for (review in reviews$Review[1:5])
-    {
+        for (review in reviews_list$Review[1:3])
+        {
+            dataset$id     <- c(dataset$id, c(id))
+            dataset$review <- c(dataset$review, c(gsub('\n', ' ', review)))
+        }
+
+        cat(paste0(percentagem, "% -> downloaded\n"))
+    }, error = function(error_condition) {
         dataset$id     <- c(dataset$id, c(id))
-        dataset$review <- c(dataset$review, c(review))
-    }
+        dataset$review <- c(dataset$review, c("NULL"))
+
+        cat(paste0(percentagem, "% -> failed\n"))
+    })
 }
 
 # Removes dummy entry
