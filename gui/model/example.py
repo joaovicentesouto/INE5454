bf59b3bf77064bb72ded305a4f0d480cee7ac19d
bonotto <bruno_mcl@hotmail.com.br>
2019-11-20 15:30:08 -0300

    Build mongo stats

--- gui/model/example.py
+++ gui/model/example.py
@@ -1,10 +1,15 @@
 from mongo_wrapper import MongoWrapper
-from cassandra_wrapper import CassandraWrapper
+# from cassandra_wrapper import CassandraWrapper
 
 m = MongoWrapper()
-res_m = m.query(categories = ['games'], stores = ['apple_apps', 'shopify_apps'], min_rating = 10, sort = 'asc', filters = ['id', 'categories', 'name'])
-print(res_m)
+# res_m = m.query(categories = ['games'], stores = ['apple_apps', 'shopify_apps'], min_rating = 10, sort = 'asc', filters = ['id', 'categories', 'name'])
+# print(res_m)
 
-c = CassandraWrapper()
-res_c = c.query(ids = [1188432952], columns = ['app_id', 'content', 'rating'], orderby = 'rating', sort = 'desc')
-print(res_c)
+query, stats = m.query(categories = ['games'], filters = ['id', 'categories', 'name', 'apple_apps', 'google_apps', 'shopify_apps'], required_stats = ['amounts', 'categories', 'prices'])
+
+print(query)
+print(stats)
+
+# c = CassandraWrapper()
+# res_c = c.query(ids = [1188432952], columns = ['app_id', 'content', 'rating'], orderby = 'rating', sort = 'desc')
+# print(res_c)
--- gui/model/mongo_wrapper.py
+++ gui/model/mongo_wrapper.py
@@ -1,6 +1,7 @@
 # import pymongo requires pip3 install pymongo
-
 from pymongo import MongoClient
+import pandas as pd
+import numpy as np
 
 class MongoWrapper:
 
@@ -8,7 +9,7 @@ class MongoWrapper:
 		self._client = MongoClient(host=['localhost:27017'])
 		print('Mongo initiation: OK')
 
-	def query(self, categories, stores = [], min_rating = 0, sort = 'desc', filters = []):
+	def query(self, categories, stores = [], min_rating = 0, sort = 'desc', filters = [], required_stats = []):
 
 		# Access database
 		apps = self._client['database']['apps']
@@ -29,7 +30,78 @@ class MongoWrapper:
 			for i in filters:
 				f[i] = 1
 
-		# Query
-		result = apps.find(q, f)
+		if 'amount' in required_stats:
+			f['apple_apps'] = 1
+			f['google_apps'] = 1
+			f['shopify_apps'] = 1
 
-		return list(result)
+		if 'categories' in required_stats:
+			f['categories'] = 1
+
+		if 'prices' in required_stats:
+			f['apps_price_plans'] = 1
+
+		# Query
+		results = apps.find(q, f)
+
+		df = pd.DataFrame(columns=filters)
+		stats = {
+			'amounts' : {
+				'apple' : 0,
+				'google' : 0,
+				'shopity' : 0,
+			},
+			'categories' : [],
+			'prices' : {
+				'sum' : 0.0,
+				'count' : 0,
+				'min' : float("inf"),
+				'max' : 0.0
+			}
+		}
+
+		for doc in results:
+
+			# Builds app information
+			row = []
+
+			for f in filters:
+				if f in doc:
+					if isinstance(doc[f], list):
+						row.append(', '.join(doc[f]))
+					elif isinstance(doc[f], dict):
+						row.append(str(doc[f]))
+					else:
+						row.append(doc[f])
+				else:
+					row.append(np.NaN)
+
+			df.loc[0 if df.empty else df.index.max() + 1] = row
+
+			# Builds app stats
+			if 'amounts' in required_stats:
+				if 'apple_apps' in doc:
+					stats['amounts']['apple'] += 1
+				if 'google_apps' in doc:
+					stats['amounts']['google'] += 1
+				if 'shopity_apps' in doc:
+					stats['amounts']['shopity'] += 1
+
+			if 'categories' in required_stats:
+				for cat in doc['categories']:
+					if cat not in categories:
+						stats['categories'].append(cat)
+
+			if 'prices' in required_stats:
+				for plan in doc['apps_price_plans']:
+					for currency in plan['currencies']:
+						stats['prices']['sum'] += currency['price']
+						stats['prices']['count'] += 1
+
+						if currency['price'] < stats['prices']['min']:
+							stats['prices']['min'] = currency['price']
+
+						if currency['price'] > stats['prices']['max']:
+							stats['prices']['max'] = currency['price']
+
+		return df, stats
